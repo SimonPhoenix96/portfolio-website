5f273368028305e67cee341807f3ae5a581a7f7f
SimonPhoenix96 <diazjustin1995@yahoo.de>
2019-12-26 17:19:15 +0000

    added innerhtml functionality, by adding custom html to content array contained in List.ts, however indexed strings should be joind with ....join("") so that html entries arent divided by "," on live website.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -14,22 +14,34 @@ import { List } from '../../classes/list';
 
 export class ListComponent implements OnInit {
 
-list: List[] = [
-  {title: "* About ...", content: "blablablabla"},
-  {title: "* Portfolio ...", content: "blablablabla"},
-{title: "* Services ...", content: "blablablabla"},
-{title: "* Contact ...", content: "blablablabla"},
+aboutText: string[] = [
+ "<img class = \"PicOfMe\" src=\"assets/img/me.jpg\" alt=\"\"> <br><br> Name: Justin Diaz <br><br> Age: 24 <br><br><br><br> Just a humble old (Business)-Informatics Student <br> trying make a living off of stuff I learned from <br> watching Indian Youtube Tutorials."
 ]
+aboutTextJoined = this.aboutText.join("");
+contactURLS: string[] = [
+  "<a href=’https://github.com/SimonPhoenix96/’>- Github</a><br><br>", "<a href=’https://de.linkedin.com/in/justin-diaz-39b21b186>- Linkedin</a><br><br>","<a href=’mailto:mail@justindiaz.xyz’>- Email</a><br><br>",
+]
+
+contactURLSJoined = this.contactURLS.join("");
+
+test: string[] = [
+  "1","2","3"
+]
+
+testJoined = this.test.join("");
+
+list: List[] = [
+  {title: "* About ...", content: this.aboutTextJoined},
+  {title: "* Projects ...",content: this.testJoined},
+  {title: "* Services ...", content: this.testJoined},
+  {title: "* Contact ...",content: this.contactURLSJoined},
+]
+
 
   constructor() { }
 
   ngOnInit() {
+
   }
-
-  toggleTitleClicked() {
-        List.titleClicked = !List.titleClicked;
-        console.log("toggleTitleClicked");
-    }
-
 
 }
