963e9c85e3b3995110dacc5af4261e85946a5f9f
SimonPhoenix96 <diazjustin1995@yahoo.de>
2019-12-26 21:25:48 +0000

    setup angulars router to handle external links, following this guide (https://medium.com/@adrianfaciu/using-the-angular-router-to-navigate-to-external-links-15cc585b7b88) and this stackblitz implementation (https://stackblitz.com/edit/angular-external-links?file=src%2Fapp%2Fhello.component.ts).

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,49 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ActivatedRouteSnapshot } from '@angular/router';
+import { InjectionToken, NgModule } from '@angular/core';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ListComponent } from './list/list.component';
 
 
-const routes: Routes = [];
+const externalUrlProvider = new InjectionToken('externalUrlRedirectResolver');
+const deactivateGuard = new InjectionToken('deactivateGuard');
+
+
+const routes: Routes = [
+
+ {
+    path: 'externalRedirect',
+    canActivate: [externalUrlProvider],
+    // We need a component here because we cannot define the route otherwise
+    component: NotFoundComponent,
+  },
+
+  {path: 'list', component: ListComponent, canDeactivate: [deactivateGuard]},
+
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule],
+   providers: [
+        {
+            provide: externalUrlProvider,
+            useValue: (route: ActivatedRouteSnapshot) => {
+
+                const externalUrl = route.paramMap.get('externalUrl');
+                window.open(externalUrl, '_self');
+            },
+        },
+        {
+          provide: deactivateGuard,
+          useValue: () => {
+            console.log('Guard function is called!')
+
+            return false;
+          }
+        },
+    ],
 })
 export class AppRoutingModule { }
