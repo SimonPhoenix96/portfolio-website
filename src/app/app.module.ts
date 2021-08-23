791a1cc5ac6cbb1a0ea39bc47b722c38d9400cce
git <git@github.com>
2021-08-23 22:20:52 +0200

    add css & html to handle new now-playign and god-says components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule,HttpClientJsonpModule } from '@angular/common/http';
 import { ExternalUrlDirective } from './external-url.directive';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SafePipe } from './safe.pipe';
@@ -13,6 +14,8 @@ import {
   MatButtonModule
 } from '@angular/material';
 import { FooterComponent } from './footer/footer.component';
+import { NowPlayingComponent } from './now-playing/now-playing.component';
+import { GodSaysComponent } from './god-says/god-says.component';
 
 
 
@@ -25,15 +28,19 @@ import { FooterComponent } from './footer/footer.component';
     SafePipe,
     NotFoundComponent,
     ExternalUrlDirective,
-    FooterComponent
-
+    FooterComponent,
+    NowPlayingComponent,
+    GodSaysComponent,
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatExpansionModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
