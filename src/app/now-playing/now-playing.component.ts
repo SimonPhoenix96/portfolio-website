a46201cacabd2ca4bbac59b8eb85fb204d3e1aa2
git <git@github.com>
2021-08-25 15:48:32 +0200

    edit structure of now-playing component template

--- src/app/now-playing/now-playing.component.ts
+++ src/app/now-playing/now-playing.component.ts
@@ -28,9 +28,13 @@ export class NowPlayingComponent implements OnInit {
       this.lastFMPlayedTitle$ = data['recenttracks']['track'][0]['name'];
       this.lastFMPlayedCover$ = data['recenttracks']['track'][0]['image'][2]['#text'];
       // this.lastFMPlayedInvidiousLink$ = "https://yewtu.be/search?q=" + this.lastFMPlayedArtist$ + "%20" + this.lastFMPlayedTitle$;
+
       this.lastFMPlayedInvidiousLink = [
-        "<a onclick=\"window.open('https://youtube.com/search?q=", data['recenttracks']['track'][0]['artist']['#text'].replace(/'/gi, "").replace(/"/gi, "") , "%20" , data['recenttracks']['track'][0]['name'].replace(/'/gi, "").replace(/"/gi, "") ,"', '_blank', 'location=yes,height=650,width=800,scrollbars=yes,status=no');\"  style=\"font-size: 90%;\" class=\"material-icons\">headphones</a>"
+        "<a  onclick=\"window.open('https://youtube.com/search?q=", data['recenttracks']['track'][0]['artist']['#text'].replace(/'/gi, "").replace(/"/gi, "") , "%20" , data['recenttracks']['track'][0]['name'].replace(/'/gi, "").replace(/"/gi, "") ,"', '_blank', 'location=no,height=650,width=800,scrollbars=yes,status=no');\"  style=\"font-size: 90%; background-color: #050d6800;\">","<span class=\"NowPlayingText\"><span style=\" font-size: 120%; margin-top=-50%;\"class=\"material-icons\">headphones   </span><br>",data['recenttracks']['track'][0]['artist']['#text'].replace(/'/gi, "").replace(/"/gi, "") ,"<br><img style=\"width: 100%;height: 10px; pointer-events:none;\" src=\"https://i.gifer.com/YdBO.gif\" ><br>",data['recenttracks']['track'][0]['name'].replace(/'/gi, "").replace(/"/gi, ""),"</span></a>"
       ]
+      // this.lastFMPlayedInvidiousLink = [
+      //   "<a onclick=\"window.open('https://youtube.com/search?q=", data['recenttracks']['track'][0]['artist']['#text'].replace(/'/gi, "").replace(/"/gi, "") , "%20" , data['recenttracks']['track'][0]['name'].replace(/'/gi, "").replace(/"/gi, "") ,"', '_blank', 'location=yes,height=650,width=800,scrollbars=yes,status=no');\"  style=\"font-size: 90%;\" class=\"material-icons\">headphones</a>"
+      // ]
       this.lastFMPlayedInvidiousLinkJoined = this.lastFMPlayedInvidiousLink.join("");
       console.log(this.lastFMPlayedInvidiousLinkJoined)
     })
