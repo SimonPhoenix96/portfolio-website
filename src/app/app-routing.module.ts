875abe57d283924c4cfa818dacad89b44cfcb8f4
simonphoenix96 <diazjustin1995@yahoo.de>
2022-12-24 13:06:41 +0100

    upgrade to angular v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
   ],
   exports: [RouterModule],
    providers: [
